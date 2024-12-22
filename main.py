81b3f7dbe0cf3e259853e93484359f664ec34480
BlueBalloonOG <122053027+BlueBalloonOG@users.noreply.github.com>
2024-12-22 11:40:01 -0600

    Update main.py
    test for intake combined, bound to x key on controller
    also the first time ive copied code from the repo and used it

--- main.py
+++ main.py
@@ -11,7 +11,8 @@
 
 #region VEXcode Generated Robot Configuration
 from vex import *
-import random
+import urandom
+
 # Brain should be defined by default
 brain=Brain()
 
@@ -23,10 +24,10 @@ right_motor_a = Motor(Ports.PORT11, GearSetting.RATIO_6_1, False)
 right_motor_b = Motor(Ports.PORT20, GearSetting.RATIO_6_1, False)
 right_drive_smart = MotorGroup(right_motor_a, right_motor_b)
 drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 295, 40, MM, 1)
+ChainIntake = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
+Arm_Score = Motor(Ports.PORT4, GearSetting.RATIO_36_1, False)
+rollers = Motor(Ports.PORT9, GearSetting.RATIO_6_1, False)
 controller_1 = Controller(PRIMARY)
-# AI Vision Color Descriptions
-# AI Vision Code Descriptions
-ai_vision_2 = AiVision(Ports.PORT2)
 
 
 # wait for rotation sensor to fully initialize
@@ -36,12 +37,9 @@ wait(30, MSEC)
 # Make random actually random
 def initializeRandomSeed():
     wait(100, MSEC)
-    
     random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
-    seed = int(random)
-initializeRandomSeed()
-
-
+    urandom.seed(int(random))
+      
 # Set random seed 
 initializeRandomSeed()
 
@@ -56,67 +54,6 @@ def play_vexcode_sound(sound_name):
 wait(200, MSEC)
 # clear the console to make sure we don't have the REPL in the console
 print("\033[2J")
-
-
-
-# define variables used for controlling motors based on controller inputs
-drivetrain_l_needs_to_be_stopped_controller_1 = False
-drivetrain_r_needs_to_be_stopped_controller_1 = False
-
-# define a task that will handle monitoring inputs from controller_1
-def rc_auto_loop_function_controller_1():
-    global drivetrain_l_needs_to_be_stopped_controller_1, drivetrain_r_needs_to_be_stopped_controller_1, remote_control_code_enabled
-    # process the controller input every 20 milliseconds
-    # update the motors based on the input values
-    while True:
-        if remote_control_code_enabled:
-            
-            # calculate the drivetrain motor velocities from the controller joystick axies
-            # left = axis2 + axis4
-            # right = axis2 - axis4
-            drivetrain_left_side_speed = controller_1.axis2.position() + controller_1.axis4.position()
-            drivetrain_right_side_speed = controller_1.axis2.position() - controller_1.axis4.position()
-            
-            # check if the value is inside of the deadband range
-            if drivetrain_left_side_speed < 5 and drivetrain_left_side_speed > -5:
-                # check if the left motor has already been stopped
-                if drivetrain_l_needs_to_be_stopped_controller_1:
-                    # stop the left drive motor
-                    left_drive_smart.stop()
-                    # tell the code that the left motor has been stopped
-                    drivetrain_l_needs_to_be_stopped_controller_1 = False
-            else:
-                # reset the toggle so that the deadband code knows to stop the left motor next
-                # time the input is in the deadband range
-                drivetrain_l_needs_to_be_stopped_controller_1 = True
-            # check if the value is inside of the deadband range
-            if drivetrain_right_side_speed < 5 and drivetrain_right_side_speed > -5:
-                # check if the right motor has already been stopped
-                if drivetrain_r_needs_to_be_stopped_controller_1:
-                    # stop the right drive motor
-                    right_drive_smart.stop()
-                    # tell the code that the right motor has been stopped
-                    drivetrain_r_needs_to_be_stopped_controller_1 = False
-            else:
-                # reset the toggle so that the deadband code knows to stop the right motor next
-                # time the input is in the deadband range
-                drivetrain_r_needs_to_be_stopped_controller_1 = True
-            
-            # only tell the left drive motor to spin if the values are not in the deadband range
-            if drivetrain_l_needs_to_be_stopped_controller_1:
-                left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
-                left_drive_smart.spin(FORWARD)
-            # only tell the right drive motor to spin if the values are not in the deadband range
-            if drivetrain_r_needs_to_be_stopped_controller_1:
-                right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
-                right_drive_smart.spin(FORWARD)
-        # wait before repeating the process
-        wait(20, MSEC)
-
-# define variable for remote controller enable/disable
-remote_control_code_enabled = True
-
-rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
 
 #endregion VEXcode Generated Robot Configuration
 
@@ -137,3 +74,11 @@ def driver_control():
             brain.screen.print("LOW BATTERY")
     
     pass
+    #intake
+    if controller_1.buttonX.pressing():
+        ChainIntake.set_max_torque(100, PERCENT)
+        rollers.set_max_torque(100, PERCENT)
+        ChainIntake.set_drive_velocity(100, PERCENT)
+        rollers.set_drive_velocity(100, PERCENT)
+    pass
+
